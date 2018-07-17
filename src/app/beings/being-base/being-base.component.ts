537dece9806d08f5128c8508fe008a9d9cf058fb
New owner <newowner@News-MacBook-Pro.local>
2018-07-17 07:52:37 -0700

    test

--- src/app/beings/being-base/being-base.component.ts
+++ src/app/beings/being-base/being-base.component.ts
@@ -3,7 +3,6 @@ import {FormBuilder} from '@angular/forms';
 import {KeyService} from '../../services/key.service';
 import {GameClockService} from '../../game-clock.service';
 import {AllItemsService} from '../../services/all-items.service';
-import {el} from '@angular/platform-browser/testing/src/browser_util';
 
 @Component({
   selector: 'vf-being-base',
@@ -44,79 +43,86 @@ export class BeingBaseComponent implements OnInit {
       x: Number(this.being.left),
       y: Number(this.being.top),
       height: 100,
-      width: 100,
-      canJump: false,
+      width: this.being.width ? this.being.width : 100,
+      jump: null,
+      jumpHeight: -200,
+      src: this.being.src,
+      src_idle: this.being.src_idle,
+      src_walking: this.being.src_walking,
     });
     this.look = this.fb.group({
       display: 'inline-block',
       position: 'absolute',
       color: 'yellow',
-      backgroundColor: this.being.backgroundColor ? this.being.backgroundColor : 'black',
       height: '100px',
       width: '100px',
       left: this.being.left + 'px',
       top: this.being.top + 'px',
+      transform: 'scaleX(-1)',
+      overflow: 'hidden',
+      'z-index': this.being.zIndex ? this.being.zIndex : 0,
     });
-
 
     // -----------------------------------
     this.keyService.pressed.subscribe(key => {
-      if (!key || this.being.type === 'platform') {
+      if (!key || this.being.type === 'platform' || !this.being.keys) {
         return;
       }
 
-      if (key.key === 'd') {
+      if (key.key === this.being.keys.right) {
         this.beingState.get('movingRight').setValue(true);
       }
 
-      if (key.key === 'a') {
+      if (key.key === this.being.keys.left) {
         this.beingState.get('movingLeft').setValue(true);
       }
-      if (key.key === 's') {
+      if (key.key === this.being.keys.down) {
         this.beingState.get('movingDown').setValue(true);
       }
-      if (key.key === 'w') {
-        if (this.beingState.value.canJump) {
+      if (key.key === this.being.keys.up) {
+        if (!this.beingState.value.jump && !this.beingState.value.movingUp) {
+          this.beingState.get('jump').setValue(this.beingState.value.y);
           this.beingState.get('movingUp').setValue(true);
+        } else {
+          console.log('denied', this.beingState.value.jump);
         }
       }
     });
 
     this.keyService.up.subscribe(key => {
-      if (!key) {
+      if (!key || this.being.type === 'platform' || !this.being.keys) {
         return;
       }
 
-      if (key.key === 'd') {
+      if (key.key === this.being.keys.right) {
         this.beingState.get('movingRight').setValue(false);
       }
 
-      if (key.key === 'a') {
+      if (key.key === this.being.keys.left) {
         this.beingState.get('movingLeft').setValue(false);
       }
-      if (key.key === 's') {
+      if (key.key === this.being.keys.down) {
         this.beingState.get('movingDown').setValue(false);
       }
-      if (key.key === 'w') {
-        this.beingState.get('movingUp').setValue(false);
+      if (key.key === this.being.keys.up) {
+        // this.beingState.get('movingUp').setValue(false);
       }
     });
-
 
 
     // -----------------------------------
     this.gameClock.clock.subscribe((time) => {
 
-      if (this.being.type === 'platform') {
+      if (this.being.type !== 'player') {
         return;
       }
 
       if (time > this.startTime) {
         let top = this.beingState.value.gravity;
-        this.beingState.value.movingUp ? top += - (this.beingState.value.gravity + this.beingState.value.speed)  : top += 0;
+        this.beingState.value.movingUp ? top += -(this.beingState.value.gravity + this.beingState.value.speed) : top += 0;
         this.beingState.value.movingDown ? top = this.beingState.value.speed : top += 0;
         let left = 0;
-        this.beingState.value.movingLeft ? left += - this.beingState.value.speed : left += 0;
+        this.beingState.value.movingLeft ? left += -this.beingState.value.speed : left += 0;
         this.beingState.value.movingRight ? left += this.beingState.value.speed : left += 0;
 
         let xBlocked = false;
@@ -126,11 +132,16 @@ export class BeingBaseComponent implements OnInit {
           if (item.id === this.id) {
             return;
           }
+          if (this.beingState.value.y < this.beingState.value.jump + this.beingState.value.jumpHeight) {
+            this.beingState.get('movingUp').setValue(false);
+          }
           if (this.beingState.value.x + left < item.beingState.value.x + item.beingState.value.width &&
             this.beingState.value.x + left + this.beingState.value.width > item.beingState.value.x &&
-            this.beingState.value.y  + top < item.beingState.value.y + item.beingState.value.height &&
+            this.beingState.value.y + top < item.beingState.value.y + item.beingState.value.height &&
             this.beingState.value.height + top + this.beingState.value.y > item.beingState.value.y) {
-            angleBlocked = true;
+            if (item.being.type === 'platform') {
+              angleBlocked = true;
+            }
             // collision support!
           }
           if (this.beingState.value.x + left < item.beingState.value.x + item.beingState.value.width &&
@@ -138,14 +149,18 @@ export class BeingBaseComponent implements OnInit {
             this.beingState.value.y < item.beingState.value.y + item.beingState.value.height &&
             this.beingState.value.height + this.beingState.value.y > item.beingState.value.y) {
             // collision support!
-            xBlocked = true;
+            if (item.being.type === 'platform') {
+              xBlocked = true;
+            }
           }
-          if (this.beingState.value.x  < item.beingState.value.x + item.beingState.value.width &&
-            this.beingState.value.x  + this.beingState.value.width > item.beingState.value.x &&
-            this.beingState.value.y  + top < item.beingState.value.y + item.beingState.value.height &&
+          if (this.beingState.value.x < item.beingState.value.x + item.beingState.value.width &&
+            this.beingState.value.x + this.beingState.value.width > item.beingState.value.x &&
+            this.beingState.value.y + top < item.beingState.value.y + item.beingState.value.height &&
             this.beingState.value.height + top + this.beingState.value.y > item.beingState.value.y) {
             // collision support!
-            yBlocked = true;
+            if (item.being.type === 'platform') {
+              yBlocked = true;
+            }
           }
         });
         if (!angleBlocked) {
@@ -157,7 +172,8 @@ export class BeingBaseComponent implements OnInit {
           this.moveTop(top);
         }
         if (yBlocked) {
-          this.beingState.get('canJump').setValue(true);
+          this.beingState.get('jump').setValue(null);
+          this.beingState.get('movingUp').setValue(false);
         }
 
       }
@@ -168,33 +184,51 @@ export class BeingBaseComponent implements OnInit {
   // -----------------------------------
 
   public getMyLook() {
-    const style = { ...this.look.value, ...{left: this.beingState.value.x + 'px', top: this.beingState.value.y + 'px'} };
+    const style = {...this.look.value, ...{left: this.beingState.value.x + 'px', top: this.beingState.value.y + 'px'}};
     return style;
   }
 
   // -----------------------------------
   public moveTop(top) {
-    console.log('top', top, this.look.value, this.beingState.value)
 
+    if (top === 0) {
+      this.being.src = this.being.src_idle;
+    }
     if (top < 0) {
-      if (top < -this.beingState.value.speed && top < -this.beingState.value.gravity) {return; }
+      if (top < -this.beingState.value.speed && top < -this.beingState.value.gravity) {
+        return;
+      }
     }
     if (top > 0) {
-      if (top > this.beingState.value.speed && top > this.beingState.value.gravity) {return; }
+      if (top > this.beingState.value.speed && top > this.beingState.value.gravity) {
+        return;
+      }
     }
-    console.log('changed top', this.beingState.get('y').value + top);
-    this.beingState.get('y').setValue(this.beingState.get('y').value + top );
+    this.beingState.get('y').setValue(this.beingState.get('y').value + top);
   }
 
   public moveLeft(left) {
-    console.log('left', left);
+    if (left === 0) {
+      this.being.src = this.being.src_idle;
+    } else {
+      this.being.src = this.being.src_walking;
+    }
     if (left < 0) {
-      if (left < -this.beingState.value.speed) { this.beingState.get('x').setValue(this.beingState.get('x').value - 1 ); console.log('left failed', left); return; }
+      if (left < -this.beingState.value.speed) {
+        this.beingState.get('x').setValue(this.beingState.get('x').value - 1);
+        console.log('left failed', left);
+        return;
+      }
+      this.look.get('transform').setValue('scaleX(-1)');
     }
     if (left > 0) {
-      if (left > this.beingState.value.speed) {  this.beingState.get('x').setValue(this.beingState.get('x').value - 1 ); console.log('left failed 2', left); return; }
+      if (left > this.beingState.value.speed) {
+        this.beingState.get('x').setValue(this.beingState.get('x').value - 1);
+        console.log('left failed 2', left);
+        return;
+      }
+      this.look.get('transform').setValue('scaleX(1)');
     }
-    console.log('changed left', this.beingState.get('x').value + left);
     this.beingState.get('x').setValue(this.beingState.get('x').value + left);
   }
 
--- src/app/stages/stage-base/stage-base.component.ts
+++ src/app/stages/stage-base/stage-base.component.ts
@@ -9,23 +9,95 @@ import {AllItemsService} from '../../services/all-items.service';
 export class StageBaseComponent implements OnInit {
 
   public beings = [
-    {type: 'player', top: '100', left: '100', backgroundColor: 'pink'},
-    {type: 'platform', top: '600', left: '100'},
-    {type: 'platform', top: '600', left: '200'},
-    {type: 'platform', top: '600', left: '300'},
-    {type: 'platform', top: '500', left: '400'},
-    {type: 'platform', top: '600', left: '500'},
-    {type: 'platform', top: '400', left: '500'},
-    {type: 'platform', top: '600', left: '600'},
-    {type: 'platform', top: '600', left: '700'},
-    {type: 'platform', top: '600', left: '800'},
-    {type: 'platform', top: '500', left: '900'},
-    {type: 'platform', top: '600', left: '000'}
+
     ];
 
   constructor(public allItemsService: AllItemsService) { }
 
   ngOnInit() {
-  }
+    this.beings.push(this.createPlayer(0, 200, {up: 'w', down: 's', right: 'd', left: 'a'}));
+    this.beings.push(this.createPlayer(0, 500, {up: 'i', down: 'k', right: 'l', left: 'j'}));
+    for (let i = 0; i < 10; i++) {
+      this.beings.push(this.createPlatform(500, 100 * i));
+    }
+    for (let i = 0; i < 10; i++) {
+      this.beings.push(this.createCoin(400, 100 * i));
+    }
+    // region trees
+    for (let i = 0; i < 10; i++) {
+      if (Math.random() * 10  > 9) {
+        this.beings.push(this.createTree(400, 100 * i));
+      }
+    }
+    for (let i = 0; i < 10; i++) {
+      if (Math.random() * 10  > 9) {
+        this.beings.push(this.createTree2(400, 100 * i));
+      }
+    }
+    for (let i = 0; i < 10; i++) {
+      if (Math.random() * 10  > 9) {
+        this.beings.push(this.createTree3(400, 100 * i));
+      }
+    }
+    for (let i = 0; i < 10; i++) {
+      if (Math.random() * 10  > 8) {
+        this.beings.push(this.createTree(300, 100 * i));
+      }
+    }
+    for (let i = 0; i < 10; i++) {
+      if (Math.random() * 10  > 8) {
+        this.beings.push(this.createTree2(300, 100 * i));
+      }
+    }
+    for (let i = 0; i < 10; i++) {
+      if (Math.random() * 10  > 8) {
+        this.beings.push(this.createTree3(300, 100 * i));
+      }
+    }
+    // region
+    for (let i = 0; i < 10; i++) {
+      this.beings.push(this.createOcean(300, 100 * i));
+    }
+    for (let i = 0; i < 10; i++) {
+      this.beings.push(this.createSky(200, 100 * i));
+    }
+    for (let i = 0; i < 10; i++) {
+      this.beings.push(this.createStars(100, 100 * i));
+    }
+    for (let i = 0; i < 10; i++) {
+      this.beings.push(this.createSpace(0, 100 * i));
+    }
+}
 
+public createPlayer(y, x, keys) {
+  return {keys, type: 'player', zIndex: 111, top: y, left: x, backgroundColor: 'pink', src: 'assets/hero_idle.gif', src_idle: 'assets/hero_idle.gif', src_walking: 'assets/hero_walking.gif'};
+}
+public createCoin(y, x) {
+  return {type: 'coin', top: y, left: x, src: 'assets/grass.png'};
+}
+
+  public createOcean(y, x) {
+    return {type: 'coin', top: y, left: x, src: 'assets/ocean.jpeg'};
+  }
+  public createSky(y, x) {
+    return {type: 'coin', top: y, left: x, src: 'assets/clouds1.jpg'};
+  }
+  public createPlatform(y, x) {
+    return  {type: 'platform', top: y, left: x, src: 'assets/grass.png'};
+  }
+  public createSpace(y, x) {
+    return  {type: 'coin', top: y, left: x, src: 'assets/space.jpg'};
+  }
+  public createStars(y, x) {
+    return  {type: 'coin', top: y, left: x, src: 'assets/stars.jpg'};
+  }
+  public createTree(y, x) {
+    return  {type: 'coin', top: y, left: x, src: 'assets/tree.png', zIndex: 111,};
+  }
+  public createTree2(y, x) {
+    return  {type: 'coin', top: y, left: x, src: 'assets/tree2.png', zIndex: 111,};
+  }
+  public createTree3(y, x) {
+    return  {type: 'coin', top: y, left: x, src: 'assets/tree3.png', zIndex: 111,};
+  }
 }
